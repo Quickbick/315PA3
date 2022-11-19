780084db0bef7bbd8b0006ed74d19a8ebfc78db5
Nathanael Ostheller <nathanael.ostheller@wsu.edu>
2022-11-19 00:31:52 -0800

    tuned output results

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # 315PA3
+
+Couldn't figure out how to get the fit results that print normally to print to file. They still will print to the terminal.
--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import pandas as pd
 import numpy as np
 import nltk
 import re
-from sklearn.feature_extraction.text import TfidVectorizer
-
+from sklearn.feature_extraction.text import TfidfVectorizer
+nltk.download('punkt')
 nltk.download('stopwords')
 from nltk.corpus import stopwords
 from sklearn.linear_model import Perceptron
@@ -19,6 +19,8 @@ y = pd.read_csv(r"./fortune-cookie-data/trainlabels.txt", header=None)
 z = pd.read_csv(r"./fortune-cookie-data/stoplist.txt", header=None)
 predData = pd.read_csv(r"fortune-cookie-data/testdata.txt", header=None)
 predLabels = pd.read_csv(r"fortune-cookie-data/testlabels.txt", header=None)
+
+outfile = open("./output.txt", "w")
 
 #Training Data Size
 rangeX = x.size
@@ -49,12 +51,12 @@ y = y['Label'].tolist()
 predLabels = predLabels['Label'].tolist()
 
 #Training Data
-v = TfidVectorizer()
+v = TfidfVectorizer()
 
 Tfidf = v.fit_transform(dX['Filt_Data'])
 
 df1 = pd.DataFrame(Tfidf.toarray(), columns=v.get_feature_names())
-print(df1)
+#print(df1)
 
 #Seperating Train and Test
 x = df1[0:rangeX]
@@ -64,11 +66,14 @@ predData = df1[rangeX:rangeX+rangePred]
 ppn = Perceptron(max_iter=20, eta0=1, random_state=0, verbose=1)
 #Fitting
 ppn.fit(x,y)
+
 #Using Test Data
+x_pred = ppn.predict(x)
 y_pred = ppn.predict(predData)
 
 #Accuracy Calculation
-print('Accuracy: %.2f' % accuracy_score(predLabels, y_pred))
+outfile.write('Accuracy: %.2f' % accuracy_score(y, x_pred))
+outfile.write(' %.2f \n' % accuracy_score(predLabels, y_pred))
 
 ########################
 #OCR
@@ -97,11 +102,11 @@ def pre_process(file):
 
         if len(l) > 2:
 
-            print(l)
-            print(l[0])
-            l1 = split(1[1][2:len(l[1])])
+            #print(l)
+            #print(l[0])
+            l1 = split(l[1][2:len(l[1])])
             dsl = len(l1)
-            print("data string length: ", len(l1))
+            #print("data string length: ", len(l1))
             if i == 0:
                 k1 = np.array(l1)
                 k = k1
@@ -109,13 +114,13 @@ def pre_process(file):
                 label = label1
             else:
                 k1 = np.array(l1)
-                k = np.append(l, k1)
-                label1 = np.array(1[2])
+                k = np.append(k, k1)
+                label1 = np.array(l[2])
                 label = np.append(label, label1)
-            print(y)
+            #print(y)
             i = i + 1
 
-        print(i)
+        #print(i)
 
     #Reshape
     k2 = k.reshape(i, dsl)
@@ -132,14 +137,16 @@ def implement_perceptron(train_data, train_labels, test_data, test_labels):
     ppn.fit(train_data, train_labels)
     #Reading Test Data
     #Using model created previously to calculate results
+    tr_pred = ppn.predict(train_data)
     t_pred = ppn.predict(test_data)
-    print(t_pred)
+    outfile.write('Accuracy: %.2f' % accuracy_score(train_labels, tr_pred))
+    outfile.write(' %.2f' % accuracy_score(test_labels, t_pred))
 
 
 #Read Train Data
-train = r"./OCR-data/ocr_train.txt"
+train = r"./OCR-data/ocr_test.txt"
 #Read Test Data
-test = r"./OCR-data/ocr_test.txt"
+test = r"./OCR-data/ocr_train.txt"
 
 #Preprocess Train Data
 train_data, train_labels = pre_process(train)
@@ -148,3 +155,4 @@ test_data, test_labels = pre_process(test)
 
 #Implemet Perceptron
 implement_perceptron(train_data, train_labels, test_data, test_labels)
+outfile.close()
